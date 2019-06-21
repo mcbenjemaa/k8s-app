b985dd89c2623f89f00da79f9cd559205efc2159
Mohamed chiheb ben jemaa <mc.benjemaa@gmail.com>
2019-06-21 16:31:54 +0100

    set spring profile

--- src/main/java/com/github/memedchiheb/k8sapp/K8sAppApplication.java
+++ src/main/java/com/github/memedchiheb/k8sapp/K8sAppApplication.java
@@ -1,13 +1,28 @@
 package com.github.memedchiheb.k8sapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class K8sAppApplication {
+	
+	@Value("${whoami.profile}")
+	private String profile;
 
+	@Value("${whoami.name}")
+	private String name;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(K8sAppApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String hello() {
+	  return "Hello " + name + " from " + profile+"!";
 	}
 
 }
